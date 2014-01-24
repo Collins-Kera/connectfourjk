7456407b7575fe75aae2587575afef884d469df1
Larsen-Jeanette <lar12054@byui.edu>
2014-01-24 09:33:53 -0500

    made classes and a separate player class

--- src/crazyconnectfour/kj/CrazyConnectFour.java
+++ src/crazyconnectfour/kj/CrazyConnectFour.java
@@ -6,18 +6,42 @@
 
 package crazyconnectfour.kj;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Kera
  */
 public class CrazyConnectFour {
-
-    /**
-     * @param args the command line arguments
-     */
+    String name;
+    String instructions = "This is game crazy connect four\n"
+            +"This game you can play against the computer or another player.\n"
+            +"To win this game you have to connect four token in a row before"
+            +"your competitor!";
     public static void main(String[] args) {
-        CrazyConnectFourmyGame = nowCrazyConnectFour();
-        //hello world!!!
+        CrazyConnectFour myGame = new CrazyConnectFour();
+        myGame.getName();
+        myGame.displayHelp();
     }
+    public void getName(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        this.name = input.next();
+       }
+    public void displayHelp(){
+        System.out.println("\nWelcome " + this.name + "\n");
+        System.out.println(this.instructions);
+       }
+public class Player{
     
+}
+public class Computer{
+   
+}
+public class Board{
+    
+}
+public class Scoreboard{
+    
+}
 }
